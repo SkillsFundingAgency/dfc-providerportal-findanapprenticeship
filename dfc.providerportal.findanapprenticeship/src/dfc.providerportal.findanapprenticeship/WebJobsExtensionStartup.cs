f2e5c4ee4e0114a7f45f3ecc9368b03c957e9070
Liam Wright <mail@liam-wright.co.uk>
2019-08-20 10:34:23 +0100

    Basic structure added

---
+++ dfc.providerportal.findanapprenticeship/src/dfc.providerportal.findanapprenticeship/WebJobsExtensionStartup.cs
@@ -0,0 +1,36 @@
+﻿using dfc.providerportal.findanapprenticeship;
+using Dfc.ProviderPortal.Packages.AzureFunctions.DependencyInjection;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+[assembly: WebJobsStartup(typeof(WebJobsExtensionStartup), "Web Jobs Extension Startup")]
+
+namespace dfc.providerportal.findanapprenticeship
+{
+    public class WebJobsExtensionStartup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            builder.AddDependencyInjection();
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            builder.Services.AddSingleton<IConfiguration>(configuration);
+            //builder.Services.Configure<CosmosDbSettings>(configuration.GetSection(nameof(CosmosDbSettings)));
+            //builder.Services.Configure<CosmosDbCollectionSettings>(configuration.GetSection(nameof(CosmosDbCollectionSettings)));
+            //builder.Services.Configure<ProviderServiceSettings>(configuration.GetSection(nameof(ProviderServiceSettings)));
+            //builder.Services.AddScoped<ICosmosDbHelper, CosmosDbHelper>();
+            //builder.Services.AddScoped<ITribalHelper, TribalHelper>();
+            //builder.Services.AddScoped<IApprenticeshipService, ApprenticeshipService>();
+
+
+        }
+    }
+}
---
+++ dfc.providerportal.findanapprenticeship/tests/dfc.providerportal.findanapprenticeship.tests/ModelTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace dfc.providerportal.findanapprenticeship.tests
+{
+    public class ModelTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
