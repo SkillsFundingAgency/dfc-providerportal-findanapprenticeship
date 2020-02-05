ac3240e129a9095007078a7a4072cea13663acb4
Steve O'Hara <github@soharadesign.com>
2020-02-05 12:55:52 +0000

    [COUR-2393] Added custom exception for handling data mapping errors

---
+++ dfc.providerportal.findanapprenticeship/src/dfc.providerportal.findanapprenticeship/Helper/DasDatamappingException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Dfc.Providerportal.FindAnApprenticeship.Helper
+{
+    public class DataMappingException : Exception
+    {
+        public DataMappingException()
+        {
+        }
+
+        public DataMappingException(string message)
+            : base(message)
+        {
+        }
+
+        public DataMappingException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
