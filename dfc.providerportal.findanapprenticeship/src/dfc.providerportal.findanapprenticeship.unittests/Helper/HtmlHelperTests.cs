d5bfc3f793e3bf5c2344784f2bfbbac308d5338d
Steve O'Hara <github@soharadesign.com>
2020-03-13 13:24:04 +0000

    [COUR-2346] Added basic HTML tag stripper

---
+++ dfc.providerportal.findanapprenticeship/src/dfc.providerportal.findanapprenticeship.unittests/Helper/HtmlHelperTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+using Dfc.Providerportal.FindAnApprenticeship.Helper;
+
+namespace Dfc.ProviderPortal.FindAnApprenticeship.UnitTests.Helper
+{
+    public class HtmlHelperTests : IDisposable
+    {
+        public HtmlHelperTests()
+        {
+
+        }
+
+        public class StripHtmlTags
+        {
+            private static string _htmlContent =
+                "<div>" +
+                    "<p>some html content with <strong>formatted</strong> text.</p>" +
+                    "<p>Second paragraph.This para has a line break <br /><blockquote>with a quote</blockquote><br /> in the middle</p>" +
+                "</div>";
+
+            [Fact]
+            public async Task ShouldRemoveAllHtmlTags()
+            {
+                // arrange
+                var expected = "some html content with formatted text. Second paragraph. This para has a line break with a quote in the middle";
+
+                // act
+                var actual = HtmlHelper.StripHtmlTags(_htmlContent);
+
+                // assert
+                Assert.Equal(expected, actual);
+            }
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ dfc.providerportal.findanapprenticeship/src/dfc.providerportal.findanapprenticeship/Helper/HtmlHelper.cs
@@ -0,0 +1,16 @@
+﻿using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+
+namespace Dfc.Providerportal.FindAnApprenticeship.Helper
+{
+    public static class HtmlHelper
+    {
+        public static string StripHtmlTags(string html)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            return doc.DocumentNode.InnerText.EnforceSpacesAfterFullstops();
+        }
+    }
+}
