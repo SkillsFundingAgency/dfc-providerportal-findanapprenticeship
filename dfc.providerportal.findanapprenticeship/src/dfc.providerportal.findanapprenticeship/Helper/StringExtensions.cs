6864b97d974ce02bb62093a66a2a246e84b61f6e
Steve O'Hara <github@soharadesign.com>
2020-03-13 13:24:04 +0000

    [COUR-2346] Added basic HTML tag stripper

---
+++ dfc.providerportal.findanapprenticeship/src/dfc.providerportal.findanapprenticeship/Helper/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Dfc.Providerportal.FindAnApprenticeship.Helper
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// COUR-2346 - Ensure spaces after fullstops are preserved once html tags are stripped out
+        /// </summary>
+        /// <param name="input">the string to parse</param>
+        /// <returns>A processed string with spaces after fullstops, except for the last fullstop</returns>
+        public static string EnforceSpacesAfterFullstops(this string input)
+        {
+            return Regex.Replace(input, "\\.(?!\\s)(?!\\.)(?!$)", ". ");
+        }
+    }
+
+}
