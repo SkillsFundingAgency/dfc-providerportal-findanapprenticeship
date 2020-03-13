0d06dd76821481c1ce2fba709b731eb12e6de7b2
Steve O'Hara <github@soharadesign.com>
2020-03-13 23:35:49 +0000

    [COUR-2346] Added feature to convert HTML line breaks to carriage returns and newlines for FAT

--- dfc.providerportal.findanapprenticeship/src/dfc.providerportal.findanapprenticeship/Helper/StringExtensions.cs
+++ dfc.providerportal.findanapprenticeship/src/dfc.providerportal.findanapprenticeship/Helper/StringExtensions.cs
@@ -13,6 +13,18 @@ namespace Dfc.Providerportal.FindAnApprenticeship.Helper
         {
             return Regex.Replace(input, "\\.(?!\\s)(?!\\.)(?!$)", ". ");
         }
+
+        /// <summary>
+        /// COUR-2346 - Replace HTML tags with newline equivalent for FAT compatibility
+        /// Course directory uses a rich HTML entry box, whereas FAT is expecting carriage returns and newlines.
+        /// TODO: Pass full sanitized HTML content to FAT so that they can show richer content
+        /// </summary>
+        /// <param name="input">the html to parse</param>
+        /// <returns>parsed html, with newlines instead of breaks/paragraphs, except for the last para</returns>
+        public static string ConvertHtmlBreaksToLineBreaks(this string input)
+        {
+            return Regex.Replace(input, "(<br>)|(<br \\/>)|(</p>)(?!$)", "\r\n\r\n");
+        }
     }
 
 }
