789f61e276d9590b6819e0b9b56d0ba13abc2f83
Steve O'Hara <github@soharadesign.com>
2020-03-24 23:13:29 +0000

    [COUR-2350] Removed redundant properties

--- dfc.providerportal.findanapprenticeship/src/dfc.providerportal.findanapprenticeship/Models/DAS/Address.cs
+++ dfc.providerportal.findanapprenticeship/src/dfc.providerportal.findanapprenticeship/Models/DAS/Address.cs
@@ -10,20 +10,12 @@ namespace Dfc.Providerportal.FindAnApprenticeship.Models.DAS
 
         public string County { get; set; }
 
-        public string Email { get; set; }
-
         public double? Latitude { get; set; }
 
         public double? Longitude { get; set; }
 
-        public string Phone { get; set; }
-
         public string Postcode { get; set; }
 
         public string Town { get; set; }
-
-        public string Website { get; set; }
-
-
     }
 }
--- dfc.providerportal.findanapprenticeship/src/dfc.providerportal.findanapprenticeship/Models/DAS/Location.cs
+++ dfc.providerportal.findanapprenticeship/src/dfc.providerportal.findanapprenticeship/Models/DAS/Location.cs
@@ -6,11 +6,9 @@ namespace Dfc.Providerportal.FindAnApprenticeship.Models.DAS
 {
     public class Location : ILocation
     {
-        public IAddress Address { get; set; }
         public int? ID { get; set; }
-        public List<int> DeliveryModes { get; set; }
-        public Guid GuidID { get; set; }
         public string Name { get; set; }
+        public IAddress Address { get; set; }
         public string Email { get; set; }
         public string Website { get; set; }
         public string Phone { get; set; }
