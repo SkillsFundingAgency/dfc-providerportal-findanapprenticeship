cfb86ac2a33b5f34742e00d81cdc42c8c9c66102
Steve O'Hara <github@soharadesign.com>
2020-03-25 13:22:52 +0000

    [COUR-2293] Added managed clients for ProviderService and ReferenceDataService

---
+++ dfc.providerportal.findanapprenticeship/src/dfc.providerportal.findanapprenticeship/Helper/ProviderService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Dfc.Providerportal.FindAnApprenticeship.Models.Providers;
+using Newtonsoft.Json;
+
+namespace Dfc.Providerportal.FindAnApprenticeship.Helper
+{
+    public class ProviderService : IProviderService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProviderService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public IEnumerable<Provider> GetActiveProviders()
+        {
+            return GetActiveProvidersAsync().Result;
+        }
+
+        public async Task<IEnumerable<Provider>> GetActiveProvidersAsync()
+        {
+            Console.WriteLine($"[{DateTime.UtcNow:G}] Cache missing or expired... Refreshing Active Providers cache");
+
+            var response = _httpClient.GetAsync($"GetActiveProviders").Result;
+
+            response.EnsureSuccessStatusCode();
+
+            var json = response.Content.ReadAsStringAsync().Result;
+            List<Provider> providers = JsonConvert.DeserializeObject<IEnumerable<Provider>>(json).ToList();
+
+            Console.WriteLine($"[{DateTime.UtcNow:G}] Loaded {providers.Count} active Providers to cache");
+
+            return providers;
+        }
+    }
+}
--- dfc.providerportal.findanapprenticeship/src/dfc.providerportal.findanapprenticeship/Helper/ProviderServiceWrapper.cs
+++ dfc.providerportal.findanapprenticeship/src/dfc.providerportal.findanapprenticeship/Helper/ProviderServiceWrapper.cs
@@ -3,11 +3,11 @@ using Dfc.Providerportal.FindAnApprenticeship.Interfaces.Helper;
 using Dfc.Providerportal.FindAnApprenticeship.Interfaces.Settings;
 using Dfc.Providerportal.FindAnApprenticeship.Models.Providers;
 using Dfc.ProviderPortal.Packages;
-using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Runtime.CompilerServices;
+using Dfc.Providerportal.FindAnApprenticeship.Models;
 using Dfc.Providerportal.FindAnApprenticeship.Settings;
 using LazyCache;
 using Microsoft.Extensions.Options;
@@ -16,17 +16,21 @@ namespace Dfc.Providerportal.FindAnApprenticeship.Helper
 {
     public class ProviderServiceWrapper : IProviderServiceWrapper
     {
+        private readonly IProviderService _client;
         private readonly IAppCache _cache;
         private readonly IProviderServiceSettings _settings;
 
         public ProviderServiceWrapper(IOptions<ProviderServiceSettings> settings, 
-            IAppCache cache)
+            IAppCache cache,
+            IProviderService client)
         {
-            Throw.IfNull(cache, nameof(cache));
             Throw.IfNull(settings, nameof(settings));
+            Throw.IfNull(cache, nameof(cache));
+            Throw.IfNull(client, nameof(client));
 
-            _cache = cache;
             _settings = settings.Value;
+            _cache = cache;
+            _client = client;
         }
 
         /// <summary>
@@ -42,14 +46,14 @@ namespace Dfc.Providerportal.FindAnApprenticeship.Helper
             }
             catch (Exception e)
             {
-                throw new ReferenceDataServiceException(UKPRN, e);
+                throw new ProviderServiceException(UKPRN, e);
             }
 
         }
 
         public IEnumerable<Provider> GetAllProviders()
         {
-            Func<IEnumerable<Provider>> activeProvidersGetter = () => PopulateActiveProvidersCache();
+            Func<IEnumerable<Provider>> activeProvidersGetter = () => _client.GetActiveProviders();
 
             try
             {
@@ -58,31 +62,12 @@ namespace Dfc.Providerportal.FindAnApprenticeship.Helper
             catch (HttpRequestException e)
             {
                 // add polly retry
-                throw new ReferenceDataServiceException(e);
+                throw new ProviderServiceException(e);
             }
             catch (Exception e)
             {
-                throw new ReferenceDataServiceException(e);
+                throw new ProviderServiceException(e);
             }
-        }
-
-
-        private IEnumerable<Provider> PopulateActiveProvidersCache()
-        {
-            Console.WriteLine($"[{DateTime.UtcNow:G}] Cache missing or expired... Refreshing Active Providers cache");
-
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _settings.ApiKey);
-            var response = client.GetAsync($"{_settings.ApiUrl}GetActiveProviders").Result;
-
-            response.EnsureSuccessStatusCode();
-
-            var json = response.Content.ReadAsStringAsync().Result;
-            List<Provider> providers = JsonConvert.DeserializeObject<IEnumerable<Provider>>(json).ToList();
-
-            Console.WriteLine($"[{DateTime.UtcNow:G}] Loaded {providers.Count} active Providers to cache");
-
-            return providers;
         }
     }
 }
