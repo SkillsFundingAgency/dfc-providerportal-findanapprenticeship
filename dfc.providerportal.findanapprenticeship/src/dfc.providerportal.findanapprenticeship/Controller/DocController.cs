7ea70cd573c01e053724e73c065819e95aa648a8
Steve O'Hara <github@soharadesign.com>
2020-03-25 17:47:46 +0000

    [COUR-2350] Added missing GetApprenticeshipsAsProviderByUkprn to swagger

--- dfc.providerportal.findanapprenticeship/src/dfc.providerportal.findanapprenticeship/Controller/DocController.cs
+++ dfc.providerportal.findanapprenticeship/src/dfc.providerportal.findanapprenticeship/Controller/DocController.cs
@@ -23,5 +23,15 @@ namespace Dfc.Providerportal.FindAnApprenticeship.Controller
             return Ok();
         }
 
+        [Route("GetApprenticeshipsAsProviderByUkprn")]
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<DASProvider>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult GetApprenticeshipsAsProviderByUkprn()
+        {
+            return Ok();
+        }
     }
 }
